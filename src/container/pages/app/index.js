4bd1a7f5989625722ee23149ecef6ca32ed015be
Erzed <feizal.reza@students.amikom.ac.id>
2021-11-04 23:17:29 +0700

    updatat 1

--- src/container/pages/app/index.js
+++ src/container/pages/app/index.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
+import { BrowserRouter as Router,Switch,Route} from "react-router-dom";
 import Login from '../login/login';
 import Register from '../register/register';
 import Dashboard from '../dashboard/dashboard';
